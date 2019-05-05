c74c4487204e088fb6af8865fef20e19fa7b9638
Guenther Raidl <raidl@ac.tuwien.ac.at>
2019-05-05 17:56:25 +0200

    multi-run-summary: configuration files now supported

--- mhlib/multi-run-summary.py
+++ mhlib/multi-run-summary.py
@@ -6,9 +6,20 @@ where the first element is some numeric value indicating the order in which the 
 the second element is a name, and the third element is a regular expression for extracting the element.
 Values >= 100 indicate that also the corresponding log-file needs to be searched and the corresponding elements
 are to be extracted from there.
+
+Example for a YAML config file:
+
+fetch:
+    '[
+    (10, "obj", r"^best obj:\s(\d+\.?\d*)"),
+    (30, "ittot", r"^total iterations:\s(\d+\.?\d*)"),
+    (20, "itbest", r"^best iteration:\s(\d+\.?\d*)"),
+    (50, "ttot", r"^total time \[s\]:\s(\d+\.?\d*)"),
+    (40, "tbest", r"best time \[s\]:\s(\d+\.?\d*)"),
+    ]'
 """
 
-import argparse
+import configargparse as p
 import glob
 import os
 import re
@@ -17,14 +28,15 @@ from typing import Any, List
 from pandas import DataFrame
 
 
-default_to_fetch = [
-    (1, 'obj', r'^best obj:\s(\d+\.?\d*)'),
-    (3, 'ittot', r'^total iterations:\s(\d+\.?\d*)'),
-    (2, 'itbest', r'^best iteration:\s(\d+\.?\d*)'),
-    (5, 'ttot', r'^total time \[s\]:\s(\d+\.?\d*)'),
-    (4, 'tbest', r'^best time \[s\]:\s(\d+\.?\d*)'),
-    (100, 'obj0', r'^0+\s(\d+.?\d*)'),
-    (101, 'obj1', r'^0+1\s(\d+.?\d*)'),
+"""Configuration of what information to extract from the out/log files."""
+fetch = [
+    (10, 'obj', r'^best obj:\s(\d+\.?\d*)'),
+    (30, 'ittot', r'^total iterations:\s(\d+\.?\d*)'),
+    (20, 'itbest', r'^best iteration:\s(\d+\.?\d*)'),
+    (50, 'ttot', r'^total time \[s\]:\s(\d+\.?\d*)'),
+    (40, 'tbest', r'^best time \[s\]:\s(\d+\.?\d*)'),
+    (110, 'obj0', r'^0+\s(\d+.?\d*)'),
+    (120, 'obj1', r'^0+1\s(\d+.?\d*)'),
 ]
 
 
@@ -87,14 +99,23 @@ def parse_files(paths, to_fetch):
 
 
 def main():
-    to_fetch = default_to_fetch
-    parser = argparse.ArgumentParser(description='Summarize results for multiple mhlib runs from their .out files.')
+    to_fetch = fetch
+    parser = p.ArgumentParser(description='Summarize results for multiple mhlib runs from their .out files.',
+                              config_file_parser_class=p.YAMLConfigFileParser)
     parser.add_argument('paths', type=str, nargs='+',
                         help='a .out file or directory (tree) containing .out files')
     parser.add_argument('--log', type=bool, default=False, help='also process corresponding .log files')
+    parser.add_argument('--fetch', type=str, default=None,
+                        help='list of tuples specifying what information to fetch')
+    parser.add_argument('-c', '--config', is_config_file=True, help='YAML-config file to be read')
+    parser._default_config_files = ['multi-run-summary.cfg']
+
     args = parser.parse_args()
-    if not args.log:
-        del default_to_fetch[-2:]
+    if args.fetch:
+        to_fetch = eval(args.fetch)
+    else:
+        if not args.log:
+            del fetch[-2:]
     df = parse_files(args.paths, to_fetch)
     print(df.to_csv(sep='\t'))
 
